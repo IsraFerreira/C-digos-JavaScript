623ca3684a2ac3380cc21f35b4ffc8b038e3ce83
Israel Ferreira <israel.fnds@hotmail.com>
2021-12-22 15:08:07 -0300

    Update script.js

--- Curso JS/aula12ex/ex015/script.js
+++ Curso JS/aula12ex/ex015/script.js
@@ -10,13 +10,52 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if(fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'foto-bebem.png')
+
+            }
+            else if (idade >= 10 && idade < 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovemm.png')
+            }
+            else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'foto-adulto.png')
+            }
+            else {
+                // idoso
+                img.setAttribute('src', 'foto-idoso.png')
+            }
+
         }
         else if(fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'foto-bebef.png')
+            }
+            else if (idade >= 10 && idade < 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovemf.png')
+            }
+            else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'foto-adulta.png')
+            }
+            else {
+                // idoso
+                img.setAttribute('src', 'foto-idosa.png')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
