1bb068ac262dc9c64552e31eb21d64f77b138df4
Israel Ferreira <israel.fnds@hotmail.com>
2021-12-23 14:26:56 -0300

    Create ambiente.js

---
+++ Curso JS/aula15/ambiente.js
@@ -0,0 +1,29 @@
+let num = [5, 8, 2, 9, 3]
+num[5] = 6 //adiciona (cria) na casa 5 o valor 6
+num.push(7) //acrescenta no ultimo elemento
+
+num.sort() //coloca todos os elementos em ordem
+
+console.log(`Nosso vetor tem ${num.length} posições`) // Mostra quantos valores tem no vetor
+
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+console.log(`O novo vetor é: ${num}`)
+
+for (var i = 0; i < num.length; i ++){
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+}
+
+/*
+for (let pos in num){
+    console.log(num[pos])
+}
+*/ // Fazer de uma forma diferente
+
+let posicao = num.indexOf(9)
+if (posicao == -1){
+    console.log('Ovalor não foi encontrado!')
+}
+else {
+    console.log(`O valor está na posição ${posicao}`)
+}
